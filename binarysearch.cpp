78218fc6450c785e14eb35e4b3e2abc685dbb1bd
NimmraTariq <nimra369tariq@gmail.com>
2025-07-10 18:33:20 +0700

    Accept input for the element to be searched

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -73,3 +73,10 @@ void binarySearch() // Start function to perform binary search
         cout << "         Pencarian Binary Search             \n";
         cout << "=============================================\n";
         cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x; // Accept input for the element to be searched
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
