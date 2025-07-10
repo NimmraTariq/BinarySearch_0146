9375725006ae834dee0f0e62977512e3f063ce18
NimmraTariq <nimra369tariq@gmail.com>
2025-07-10 18:29:40 +0700

    Read the number of elements from the user

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,3 +4,10 @@ using namespace std;
 int element[10]; // Declare an array to hold up to 10 elements
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang; // Read the number of elements from the user
